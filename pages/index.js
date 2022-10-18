11cb685317828733a774cd501a0deec816f743ec
Your Name <massingkkevin@gmail.com>
2022-10-18 12:49:28 +0200

    test gallery

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default class Home extends Component {
         <article>
             <Link href="/galerie">Galerie</Link>
           
-          ))}
+          
             
          
           
